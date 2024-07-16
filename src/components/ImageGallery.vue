c2118140630c595e009acaeefd45cbe18c1570ae
Sergey-Okey <sergeyborisov_1@vk.com>
2024-07-16 11:29:52 +0300

    adaptive

--- src/components/ImageGallery.vue
+++ src/components/ImageGallery.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="gallery-container">
     <div class="thumbnails">
-      <div v-for="(image, index) in images" :key="index" class="thumbnail" @click="selectImage(index)">
+      <div v-for="(image, index) in images" :key="index" class="thumbnail" @click="selectImage(index)"
+        :class="{ active: selectedImageIndex === index }">
         <img :src="getImageSrc(image)" :alt="'Image ' + index" />
       </div>
     </div>
@@ -42,46 +43,83 @@ export default {
   height: auto;
   gap: 10px;
 
-  @include breakpoint ('mobile') {
+  @include breakpoint('mobile') {
     flex-wrap: wrap-reverse;
   }
 
-  @include breakpoint ('mobile', 'wide') {}
+  @include breakpoint('mobile', 'wide') {}
 
-  @include breakpoint ('tablet', 'medium') {}
+  @include breakpoint('tablet', 'medium') {}
 
-  @include breakpoint ('tablet', 'wide') {
+  @include breakpoint('tablet', 'wide') {
     flex-wrap: nowrap;
   }
 
-  @include breakpoint ('desktop', 'medium') {}
+  @include breakpoint('desktop', 'medium') {}
 
-  @include breakpoint ('desktop') {}
+  @include breakpoint('desktop') {}
 
-  @include breakpoint ('desktop', 'wide') {}
+  @include breakpoint('desktop', 'wide') {}
 
   .large-image {
+    position: relative;
     width: 100%;
     max-width: 765px;
 
-    @include breakpoint ('mobile') {}
+    @include breakpoint('mobile') {}
 
-    @include breakpoint ('mobile', 'wide') {}
+    @include breakpoint('mobile', 'wide') {}
 
-    @include breakpoint ('tablet', 'medium') {}
+    @include breakpoint('tablet', 'medium') {}
 
-    @include breakpoint ('tablet', 'wide') {}
+    @include breakpoint('tablet', 'wide') {}
 
-    @include breakpoint ('desktop', 'medium') {
+    @include breakpoint('desktop', 'medium') {
       max-width: 500px;
     }
 
-    @include breakpoint ('desktop') {
+    @include breakpoint('desktop') {
       max-width: 500px;
     }
 
-    @include breakpoint ('desktop', 'wide') {
+    @include breakpoint('desktop', 'wide') {
       max-width: 765px;
+    }
+
+    &::after {
+      position: absolute;
+      width: 100%;
+      content: 'LEXMER';
+      color: white;
+      top: 0;
+      left: 0;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 500;
+      font-family: 'Bebas';
+      font-size: 30px;
+      margin-top: 30%;
+      letter-spacing: 0.5em;
+      text-align: center;
+
+      @include breakpoint('mobile') {
+        font-size: 20px;
+      }
+
+      @include breakpoint('mobile', 'wide') {
+        font-size: 25px;
+      }
+
+      @include breakpoint('tablet') {
+        font-size: 35px;
+      }
+
+      @include breakpoint('desktop') {
+        font-size: 35px;
+      }
+
+      @include breakpoint('desktop', 'wide') {}
     }
 
     img {
@@ -95,26 +133,66 @@ export default {
     grid-template-columns: 1fr;
     gap: 10px;
 
-    @include breakpoint ('mobile') {
+    @include breakpoint('mobile') {
       grid-template-columns: 1fr 1fr;
       grid-template-rows: 1fr 1fr;
     }
 
-    @include breakpoint ('mobile', 'wide') {
+    @include breakpoint('mobile', 'wide') {
       grid-template-columns: 1fr 1fr 1fr 1fr;
       grid-template-rows: 1fr;
     }
 
-    @include breakpoint ('tablet', 'wide') {
+    @include breakpoint('tablet', 'wide') {
       grid-template-columns: 1fr;
       grid-template-rows: 1fr 1fr 1fr 1fr;
       max-width: 168px;
     }
 
     .thumbnail {
+      position: relative;
       cursor: pointer;
       width: 100%;
-      height: 100%;
+      height: auto;
+      background: #EDF0F2;
+
+      &::after {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        content: 'LEXMER';
+        color: white;
+        top: 0;
+        left: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-weight: 400;
+        font-family: 'Bebas';
+        letter-spacing: 0.5em;
+        text-align: center;
+
+        @include breakpoint('mobile') {
+          font-size: 10px;
+        }
+
+        @include breakpoint('mobile', 'wide') {
+          font-size: 10px;
+        }
+
+        @include breakpoint('desktop', 'medium') {
+          font-size: 15px;
+        }
+
+        @include breakpoint('desktop') {}
+
+        @include breakpoint('desktop', 'wide') {}
+      }
+
+      &.active {
+        outline: solid 2px black;
+        padding: 5px;
+      }
 
       img {
         width: 100%;
